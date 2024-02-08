ed094b238c19eb9889e48114c79d7074ac50d0df
Filipe Pileggi <119987400+filipepileggi@users.noreply.github.com>
2024-02-07 22:25:33 -0300

    Add files via upload
    O operador do módulo
    
    O operador do módulo, %, retorna o resto de uma operação de divisão. Pode ser aplicado a tipos de  ponto flutuante e a números inteiros. O programa de exemplo a seguir demonstra o%:

---
+++ operadorDeModulo.java
@@ -0,0 +1,12 @@
+public class operadorDeModulo {
+
+    public static void main (String[] args) {
+
+        int x = 15;
+        int int_resto = x % 10;
+        System.out.println("Resultado de 15 % 10=" + int_resto);
+        double d = 15.25;
+        double double_resto = d % 10;
+        System.out.println("Resultado de 15.25 % 10=" + double_resto);
+    }
+}
